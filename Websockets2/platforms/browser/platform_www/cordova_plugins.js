1d830bb50f4cca8bc904aa88d28efe2792bf172e
JaviHort <35100351+JaviHort@users.noreply.github.com>
2018-05-16 17:12:49 +0200

    Aplicación cliente finalizada y subida

--- Websockets2/platforms/browser/platform_www/cordova_plugins.js
+++ Websockets2/platforms/browser/platform_www/cordova_plugins.js
@@ -1,19 +1,42 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.tlantic.plugins.socket/www/socket.js",
-        "id": "com.tlantic.plugins.socket.Socket",
-        "pluginId": "com.tlantic.plugins.socket",
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "pluginId": "cordova-plugin-camera",
         "clobbers": [
-            "window.tlantic.plugins.socket"
+            "Camera"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/src/browser/CameraProxy.js",
+        "id": "cordova-plugin-camera.CameraProxy",
+        "pluginId": "cordova-plugin-camera",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.tlantic.plugins.socket": "0.3.0",
-    "cordova-plugin-websocket": "0.12.2"
+    "cordova-plugin-websocket": "0.12.2",
+    "cordova-plugin-compat": "1.2.0",
+    "cordova-plugin-camera": "2.4.1"
 }
 // BOTTOM OF METADATA
 });
--- Websockets2/platforms/browser/www/js/miWebSocket.js
+++ Websockets2/platforms/browser/www/js/miWebSocket.js
@@ -2,32 +2,18 @@ var miWebSocket = {
 
   // Application Constructor
   initialize: function() {
-    var IP = "0.0.0.0"
-    var PORT = "0000"
-    var ws = null
-
-    this.bindEvents();
-  },
-  // Bind Event Listeners
-  //
-  // Bind any events that are required on startup. Common events are:
-  // 'load', 'deviceready', 'offline', and 'online'.
-  bindEvents: function() {
-    document.addEventListener('deviceready', this.init, false);
-  },
-
-  init: function() {
-    console.log("init() llamado");
-    IP = "10.82.145.181"
-    PORT = "9797"
-    document.querySelector('#mostrarDatos').innerHTML = "Preparado para conectar en ws://" + IP + ':' + PORT;
+    IP = "0.0.0.0";
+    PORT = "0000";
+    ws = null;
+    estadoWebSocket = "desconectado"
+    lan_s = "";
+    lan_d = "";
   },
 
   doConnect: function() {
+
     ws = new WebSocket('ws://' + IP + ':' + PORT);
     //Definimos las funciones del WebSocket
-    console.log(IP);
-    console.log(PORT);
     ws.onopen = function(evt) {
       miWebSocket.onOpen(evt)
     };
@@ -43,45 +29,56 @@ var miWebSocket = {
   },
 
   onOpen: function(evt) {
-    document.querySelector('#mostrarDatos').innerHTML = "Socket conectado con exito";
+    estadoWebSocket = "conectado";
+    miWebSocket.sendIdiomas();
+    ons.notification.alert('Conexión establecida con: (' + IP + ' : ' +  PORT + ')');
   },
 
   onClose: function(evt) {
-    console.log("Cerramos socket");
-    console.log(evt.data);
-    document.querySelector('#mostrarDatos').innerHTML = "Cerrando socket...";
+    estadoWebSocket = "desconectado";
+    mi_app.cerrarPantallaCarga();
+    document.getElementById('popover').style.visibility = 'visible';
+    document.getElementById('mask-popover').style.visibility = 'visible';
+    document.getElementById('barra-carga').style.visibility = "hidden";
   },
 
   onMessage: function(evt) {
-    document.querySelector('#mostrarDatos').innerHTML = evt.data;
+    document.querySelector('#texto-traduccion').innerHTML = evt.data;
+    document.getElementById('barra-carga').style.visibility = "hidden";
   },
 
   onError: function(evt) {
-    console.log("Error");
-    console.log(evt.data);
-    document.querySelector('#mostrarDatos').innerHTML = "Error de socket";
-    ws.close();
+    estadoWebSocket = "desconectado";
+    ons.notification.alert('Error de conexión con el IP/PUERTO asignado.');
+    mi_app.cerrarPantallaCarga();
   },
 
   doSend: function(message) {
     ws.send(message);
   },
 
-  sendText: function() {
-    var lan_s = document.getElementById('lang_src').value;
-    var lan_d = document.getElementById('lang_dest').value;
-    document.querySelector('#mostrarDatos').innerHTML = "Idioma establecido";
-    miWebSocket.doSend(lan_s + '_' + lan_d);
+  sendIdiomas: function() {
+    if ((lan_s != document.getElementById('lang_src').value) || (lan_d != document.getElementById('lang_dest').value)) {
+      lan_s = document.getElementById('lang_src').value;
+      lan_d = document.getElementById('lang_dest').value;
+      miWebSocket.doSend(lan_s + '_' + lan_d);
+      document.querySelector('#titulo-traduccion').innerHTML = 'Traduciendo ("' + lan_s + '" a "' + lan_d + '"):';
+      document.querySelector('#texto-traduccion').innerHTML = "";
+      document.getElementById('encuadre').style.backgroundImage = "url('img/image.png')";
+    }
+    mi_app.cerrarPantallaCarga();
+    document.getElementById('popover').style.visibility = 'hidden';
+    document.getElementById('mask-popover').style.visibility = 'hidden';
   },
 
-  sendImage: function() {
+  sendImageCamera: function() {
     var opciones_foto = {
-      quality: 60,
+      quality: 100,
       sourceType: Camera.PictureSourceType.CAMERA,
       destinationType: Camera.DestinationType.DATA_URL, //Base64
       encodingType: 0, //JPEG
-      targetWidth: 1500,
-      targetHeight: 1125,
+      targetWidth: 3000,
+      targetHeight: 2250,
       correctOrientation: true,
       cameraDirection: 1
     };
@@ -89,21 +86,62 @@ var miWebSocket = {
     navigator.camera.getPicture(function(imageData) {
       ws.send("Enviando imagen...");
       ws.send(imageData);
+      document.querySelector('#texto-traduccion').innerHTML = "";
+      document.getElementById('encuadre').style.backgroundImage = "url('data:image/png;base64," + imageData + "')";
+      document.getElementById('barra-carga').style.visibility = "visible";
     }, function() {
-      document.querySelector('#mostrarDatos').innerHTML = "¡Error al tomar la foto!";
+
     }, opciones_foto);
   },
 
-  setIPPORT: function() {
-    IP = document.getElementById('IP_setter').value;
-    PORT = document.getElementById('PORT_setter').value;
-    console.log(IP);
-    console.log(PORT);
-    document.querySelector('#mostrarDatos').innerHTML = "IP y puerto aceptados";
+  sendImageGallery: function() {
+    var opciones_foto = {
+      quality: 100,
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: Camera.DestinationType.DATA_URL, //Base64
+      encodingType: 0, //JPEG
+      targetWidth: 3000,
+      targetHeight: 2250,
+      correctOrientation: true,
+      cameraDirection: 1
+    };
+
+    navigator.camera.getPicture(function(imageData) {
+      ws.send("Enviando imagen...");
+      ws.send(imageData);
+      document.querySelector('#texto-traduccion').innerHTML = "";
+      document.getElementById('encuadre').style.backgroundImage = "url('data:image/png;base64," + imageData + "')";
+      document.getElementById('barra-carga').style.visibility = "visible";
+    }, function() {
+
+    }, opciones_foto);
   },
 
   doDisconnect: function() {
+    estadoWebSocket = "desconectado";
     ws.close();
+  },
+
+  setParametros: function() {
+    //Comprobamos si hemos seleccionado otra IP y/o Puerto
+    if ((estadoWebSocket == "conectado") && ((document.getElementById('IP_setter').value != IP) || (document.getElementById('PORT_setter').value != PORT))) {
+      miWebSocket.doDisconnect();
+    }
+
+    //Comprobamos que tenemos el socket desconectado
+    if (estadoWebSocket == "desconectado") {
+      IP = document.getElementById('IP_setter').value;
+      PORT = document.getElementById('PORT_setter').value;
+      try {
+        miWebSocket.doConnect();
+      } catch (err) {
+        //Block of code to handle errors
+        ons.notification.alert('IP o PUERTO no válidos.');
+        mi_app.cerrarPantallaCarga();
+      }
+    } else {
+      miWebSocket.sendIdiomas();
+    }
   }
 
 };
